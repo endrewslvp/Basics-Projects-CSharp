f6d79c40086ca6860525c39594809178c8121f76
Endrew <145734717+endrewslvp@users.noreply.github.com>
2024-02-25 19:42:32 -0300

    Adicionando Tabuada
    Este projeto é um sistema que gera a tabuada a partir de um valor inserido pelo usuário.

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TABUADA -----\n");
+            Console.WriteLine("Este sistema é responsável por gerar a tabuada de um valor inserido pelo usuário.\n");
+            Console.Write("Insira um número: ");
+            float num = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            multtable(num);
+            Console.ReadLine(); 
+        }
+        static void multtable (float num)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+    }
+}
