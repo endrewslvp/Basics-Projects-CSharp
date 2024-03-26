32121e7538e9831823124ae53b88a4d502296505
Endrew <145734717+endrewslvp@users.noreply.github.com>
2024-03-25 21:41:19 -0300

    IMC v2
    Inserindo novo projeto de IMC orientado a objetos.

---
+++ IMC v2/IMC v2/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC_v2
+{
+    internal class Pessoa
+    {
+        public double weight;
+        public double height;
+
+        public double imc()
+        {
+            return Math.Round(weight / Math.Pow(height, 2),2);
+        }
+
+        public string status()
+        {
+            if (imc() < 18.5) return "Abaixo do peso";
+            else if (imc() < 25) return "Peso normal";
+            else if (imc() < 30) return "Acima do peso";
+            else if (imc() < 35) return "Obesidade I";
+            else if (imc() < 40) return "Obesidade II";
+            else return "Obesidade III";
+        }
+
+        public void message()
+        {
+            Console.WriteLine("Seu IMC é: "+imc());
+            Console.WriteLine("Resultado: "+status());
+        }
+    }
+}
---
+++ IMC v2/IMC v2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            Console.WriteLine("Bem-vindo(a) ao sistema IMC.");
+            Console.Write("Insira seu peso em kilogramas (Kg): ");
+            obj.weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira sua altura em metros: ");
+            obj.height = Convert.ToDouble(Console.ReadLine());
+            obj.message();
+        }
+    }
+}
